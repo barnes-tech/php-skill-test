5b0af04dbcb3f73e6eb94ca09676615ad1852b87
Sean Barnes <60889084+barnes-tech@users.noreply.github.com>
2022-03-24 08:58:29 +0000

    Add files via upload

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,6 @@
-
+function disableButton() {
+        var btn = document.getElementById('btn');
+        btn.disabled = true;
+        btn.innerText = 'Posting...';
+        console.log('it did work');
+    }
