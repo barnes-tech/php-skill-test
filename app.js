7e29010f125366abad06a78a5253cc4af6d68b80
Sean Barnes <60889084+barnes-tech@users.noreply.github.com>
2022-03-24 08:58:06 +0000

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function disableButton() {
-        var btn = document.getElementById('btn');
-        btn.disabled = true;
-        btn.innerText = 'Posting...';
-        console.log('it did work');
-    }
